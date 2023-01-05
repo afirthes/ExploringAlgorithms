1be74e0d3d54fc42054f88d8d6ac399003b5c572
Sergio <afirthes@gmail.com>
2023-01-05 15:55:27 +0300

    Initial Commit

---
+++ ExploringAlgorithms/ExploringAlgorithms.h
@@ -0,0 +1,18 @@
+//
+//  ExploringAlgorithms.h
+//  ExploringAlgorithms
+//
+//  Created by Afir Thes on 05.01.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ExploringAlgorithms.
+FOUNDATION_EXPORT double ExploringAlgorithmsVersionNumber;
+
+//! Project version string for ExploringAlgorithms.
+FOUNDATION_EXPORT const unsigned char ExploringAlgorithmsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ExploringAlgorithms/PublicHeader.h>
+
+
